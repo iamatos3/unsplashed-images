1d643c241d0c93b84c94ab666a18bb849ffa41e4
iamatos3 <matosian93@gmail.com>
2023-08-10 14:58:36 -0400

    Setup the Unsplash API and functionality to populate photos
    - Obtained correct URL
    - Setup the React Query
    - Rendered all Images through Gallery component
    - Updated the searchValue globally
    - Updated the Dark Mode feature within project as preferred by
    user via local storage
    - Created .env file to protect API Key ID

--- src/context.jsx
+++ src/context.jsx
@@ -2,18 +2,30 @@ import { createContext, useContext, useState, useEffect } from "react";
 
 const AppContext = createContext();
 
+const getInitialDarkMode = () => {
+  const prefersDarkMode = window.matchMedia(
+    '(prefers-color-scheme:dark)'
+  ).matches;
+  const storedDarkMode = localStorage.getItem('darkMode') === 'true';
+  return storedDarkMode || prefersDarkMode;
+}
+
 export const AppProvider = ({ children }) => {
-  const [isDarkMode, setIsDarkMode] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(getInitialDarkMode());
+  const [searchTerm, setSearchTerm] = useState('elephant');
 
   const toggleDarkMode = () => {
     const newDarkMode = !isDarkMode;
     setIsDarkMode(newDarkMode);
-    const body = document.querySelector('body');
-    body.classList.toggle('dark-mode', newDarkMode);
+    localStorage.setItem('darkMode', newDarkMode);
   };
 
+  useEffect(() => {
+    document.body.classList.toggle('dark-mode', isDarkMode);
+  }, [isDarkMode]);
+
   return (
-    <AppContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+    <AppContext.Provider value={{ isDarkMode, toggleDarkMode, searchTerm, setSearchTerm }}>
       {children}
     </AppContext.Provider>
   );
